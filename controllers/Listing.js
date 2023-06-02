c92058669669e756c94937665a804f3c5f0352d1
MoonswapFinance <ekwuemerapheal152@gmail.com>
2023-06-02 20:34:23 +0100

    updated listing filter

--- controllers/Listing.js
+++ controllers/Listing.js
@@ -16,8 +16,8 @@ export const getAllListing = async (req, res) => {
 
 export const getListing = async (req, res) => {
   try {
-    const { id } = req.params;
-    const Listing = ListingModel.findById(id);
+    const { slug } = req.params;
+    const Listing = await ListingModel.findOne({ pname: slug });
     res.status(200).json(Listing);
   } catch (error) {
     console.log({ error: error.message });
