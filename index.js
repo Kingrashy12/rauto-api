083e3259192e8dd3a420674db07bee19b342e7a8
MoonswapFinance <ekwuemerapheal152@gmail.com>
2023-06-04 03:12:52 +0100

    updated limit

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const app = express();
 dotenv.config();
 
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "80mb" }));
+app.use(bodyParser.json({ limit: "80mb" }));
 app.use(
   bodyParser.urlencoded({
-    limit: "50mb",
+    limit: "80mb",
     extended: true,
     parameterLimit: 50000,
   })
